13bc9ca4370222b659232ac0e961edd00d895358
Grzegorz Pabian <greg.pabian@gmail.com>
2014-04-04 11:49:45 +0200

    initial commit

---
+++ tests/test.js
@@ -0,0 +1,22 @@
+var fs = require('fs'),
+    path = require('path'),
+    combine = require('../lib'),
+    sources = {},
+    combined;
+
+fs.readdirSync(__dirname).forEach(function (file) {
+    if (file === 'tests.js') return;
+
+    sources[file] = fs.readFileSync(path.join(__dirname, file)).toString();
+});
+
+combined = combine(
+    sources['template.html'],
+    sources['1.html'],
+    sources['2.html'],
+    sources['3.html']
+);
+
+console.log(combined);
+
+// TODO prepare proper test cases
