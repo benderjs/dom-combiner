6d440b7c07af8d73c0624462d909f405e288f082
Grzegorz Pabian <greg.pabian@gmail.com>
2014-05-06 14:08:55 +0200

    added node-style callback to combine function

--- tests/test.js
+++ tests/test.js
@@ -10,8 +10,18 @@ fs.readdirSync(__dirname).forEach(function (file) {
     sources[file] = fs.readFileSync(path.join(__dirname, file)).toString();
 });
 
-combined = combine(sources['template.html'], sources['1.html'], sources['2.html'], sources['3.html']);
+combine(sources['template.html'], sources['1.html'], sources['2.html'], sources['3.html'], function (err, combined) {
+    if (err) return console.err(err);
+    console.log(combined);
+});
 
-console.log(combined);
+console.log(
+    combine(
+        sources['template.html'],
+        sources['1.html'],
+        sources['2.html'],
+        sources['3.html']
+    )
+);
 
 // TODO prepare proper test cases
