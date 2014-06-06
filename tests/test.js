7ccc75d3b5ca0d478c92a825ceda32523a6db1cd
Grzegorz Pabian <greg.pabian@gmail.com>
2014-06-06 14:45:34 +0200

    Applied company code style; Added JSCS configuration file.

--- tests/test.js
+++ tests/test.js
@@ -1,27 +1,37 @@
-var fs = require('fs'),
-    path = require('path'),
-    combine = require('../lib'),
-    sources = {},
-    combined;
+var fs = require( 'fs' ),
+	path = require( 'path' ),
+	combine = require( '../lib' ),
+	sources = {},
+	combined;
 
-fs.readdirSync(__dirname).forEach(function (file) {
-    if (file === 'tests.js') return;
+fs.readdirSync( __dirname ).forEach( function( file ) {
+	if ( file === 'tests.js' ) {
+		return;
+	}
 
-    sources[file] = fs.readFileSync(path.join(__dirname, file)).toString();
-});
+	sources[ file ] = fs.readFileSync( path.join( __dirname, file ) ).toString();
+} );
 
-combine(sources['template.html'], sources['1.html'], sources['2.html'], sources['3.html'], function (err, combined) {
-    if (err) return console.err(err);
-    console.log(combined);
-});
+combine(
+	sources[ 'template.html' ],
+	sources[ '1.html' ],
+	sources[ '2.html' ],
+	sources[ '3.html' ],
+	function( err, combined ) {
+		if ( err ) {
+			return console.err( err );
+		}
+
+		console.log( combined );
+	} );
 
 console.log(
-    combine(
-        sources['template.html'],
-        sources['1.html'],
-        sources['2.html'],
-        sources['3.html']
-    )
+	combine(
+		sources[ 'template.html' ],
+		sources[ '1.html' ],
+		sources[ '2.html' ],
+		sources[ '3.html' ]
+	)
 );
 
 // TODO prepare proper test cases
